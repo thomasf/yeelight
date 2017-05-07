71c0e504bf4457fe06960dc51f2c09c5f36d0049
Thomas Frössman <thomasf@jossystem.se>
2017-05-07 19:36:36 +0200

    Add initial code

---
+++ cmd/yeelight-mqtt/yeelight-mqtt.go
@@ -0,0 +1,47 @@
+// yeelmqtt server
+package main
+
+import (
+	"flag"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/thomasf/lg"
+	_ "github.com/thomasf/lg/pkg/prometheus"
+	"github.com/thomasf/yeelight/pkg/mqtt"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	lg.SetSrcHighlight("yeelight/cmd", "yeelight/pkg")
+	lg.CopyStandardLogTo("warning")
+
+	var netifName, mqttConnStr, metricsAddr string
+	flag.StringVar(&netifName, "if", "", "network interface for yl multicast network")
+	flag.StringVar(&mqttConnStr, "mqtt", "mqtt://guest:guest@127.0.0.1", "mqtt connection string")
+	flag.StringVar(&metricsAddr, "metricsAddr", ":8080", "metrics listening addr")
+
+	flag.Parse()
+	if netifName == "" {
+		lg.Errorln("network interface name (-if) not specified")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if metricsAddr != "" {
+		http.Handle("/metrics", promhttp.Handler())
+		go func(addr string) {
+			lg.Fatal(http.ListenAndServe(addr, nil))
+		}(metricsAddr)
+	}
+
+	service := mqtt.MQTTService{
+		MQTTConnectionStr: mqttConnStr,
+		MulticastIF:       netifName,
+	}
+	lg.Error(service.Start())
+
+}
